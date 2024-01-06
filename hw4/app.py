b6e6dcbf767bf14230ed5417c17829d45a81a746
TainanKyle <kylehuang121@gmail.com>
2024-01-06 16:52:54 +0800

    add tracking model

--- hw4/app.py
+++ hw4/app.py
@@ -1,5 +1,20 @@
-from flask import Flask, Response
 import cv2
+import numpy as np
+
+from api.deepsort import DeepSORTTracker
+from api.yolo import YOLOPersonDetector
+from flask import Flask, Response
+
+# constants
+YOLO_MODEL = "./checkpoints/yolov7x.pt"
+REID_MODEL = "./checkpoints/ReID.pb"
+MAX_COS_DIST = 0.5
+MAX_TRACK_AGE = 100
+
+detector = YOLOPersonDetector()
+detector.load(YOLO_MODEL)
+tracker = DeepSORTTracker(REID_MODEL, MAX_COS_DIST, MAX_TRACK_AGE)
+
 
 def capture_video():
     # 開啟攝像頭
@@ -10,6 +25,25 @@ def capture_video():
         if not ret:
             return "Camera cannot capture video"
             break
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # process YOLO detections
+        detections = detector.detect(frame)
+        try:
+            bboxes, scores, _ = np.hsplit(detections, [4, 5])
+            bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, :2]
+            n_objects = detections.shape[0]
+        except ValueError:
+            bboxes = np.empty(0)
+            scores = np.empty(0)
+            n_objects = 0
+
+        # track targets by refining with DeepSORT
+        tracker.track(frame, bboxes, scores.flatten())
+
+        # write to output video
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
         # 將frame轉換為JPEG格式
         _, buffer = cv2.imencode('.jpg', frame)
